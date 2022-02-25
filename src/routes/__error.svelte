b3a3fb549c4f2bced23e596d3c420957f041940b
ehsan <ehsan2003.2003.382@gmail.com>
2022-02-25 18:21:44 +0330

    adds an error page which redirects all not found errors to /

---
+++ src/routes/__error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" context="module">
+	import type { ErrorLoad } from '@sveltejs/kit';
+
+	export const load: ErrorLoad = ({ status }) => {
+		if (status === 404) {
+			return {
+				redirect: '/'
+			};
+		}
+	};
+</script>
