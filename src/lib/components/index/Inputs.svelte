5220ea3d134b2b09dd434514bf38735234f2c33d
ehsan <ehsan2003.2003.382@gmail.com>
2022-04-07 17:54:50 +0430

    removes grid from the inputs

--- src/lib/components/index/Inputs.svelte
+++ src/lib/components/index/Inputs.svelte
@@ -31,48 +31,52 @@
 	}
 </script>
 
-<div class="grid grid-cols-[5fr_2fr] gap-y-1 w-64 mt-5 items-stretch">
-	<div class="border border-black  rounded-r-[4px] overflow-hidden">
-		<input
-			type="number"
-			min="0"
-			bind:value={bonus}
-			class="block w-full  placeholder:text-sm h-7  px-2 transition-colors placeholder:transition-colors outline-none"
-			class:placeholder:text-red-600={bonusInvalid && isFirstClicked}
-			class:text-red-600={bonusInvalid && isFirstClicked}
-			class:bg-red-100={bonusInvalid && isFirstClicked}
-			placeholder="درصد سود"
-		/>
-	</div>
-	<div class="rounded-l-[4px] overflow-hidden">
-		<select
-			class="bg-black  text-white px-2 h-full w-full block text-center text-sm tracking-wide"
-			name=""
-			id=""
-			bind:value={bonusDuration}
-		>
-			{#each timeUnits as unit}
-				<option value={unit.days}>{unit.label}</option>
-			{/each}
-		</select>
+<div class="flex flex-col gap-y-1 w-64 mt-5 items-stretch">
+	<div class="flex">
+		<div class="border border-black  rounded-r-[4px] overflow-hidden w-[71%]">
+			<input
+				type="number"
+				min="0"
+				bind:value={bonus}
+				class="block w-full  placeholder:text-sm h-7  px-2 transition-colors placeholder:transition-colors outline-none"
+				class:placeholder:text-red-600={bonusInvalid && isFirstClicked}
+				class:text-red-600={bonusInvalid && isFirstClicked}
+				class:bg-red-100={bonusInvalid && isFirstClicked}
+				placeholder="درصد سود"
+			/>
+		</div>
+		<div class="rounded-l-[4px] overflow-hidden w-[29%]">
+			<select
+				class="bg-black  text-white px-2 h-full w-full block text-center text-sm tracking-wide"
+				name=""
+				id=""
+				bind:value={bonusDuration}
+			>
+				{#each timeUnits as unit}
+					<option value={unit.days}>{unit.label}</option>
+				{/each}
+			</select>
+		</div>
 	</div>
 
-	<div class="border border-black  rounded-r-[4px] overflow-hidden ">
-		<input
-			type="number"
-			min="0"
-			class="block  outline-none w-full placeholder:text-sm h-7  px-2 transition-colors placeholder:transition-colors"
-			placeholder="سرمایه ورودی به دلار"
-			class:placeholder:text-red-600={initialBudgetInvalid && isFirstClicked}
-			class:text-red-600={initialBudgetInvalid && isFirstClicked}
-			class:bg-red-100={initialBudgetInvalid && isFirstClicked}
-			bind:value={initialBudget}
-		/>
-	</div>
-	<div
-		class="bg-black  text-white px-2 h-full w-full text-center text-sm tracking-wide rounded-l-[4px] overflow-hidden flex items-center justify-center font-mono font-bold"
-	>
-		<span>$</span>
+	<div class="flex ">
+		<div class="border border-black  rounded-r-[4px] overflow-hidden w-[71%]">
+			<input
+				type="number"
+				min="0"
+				class="block  outline-none placeholder:text-sm h-7  px-2 transition-colors placeholder:transition-colors"
+				placeholder="سرمایه ورودی به دلار"
+				class:placeholder:text-red-600={initialBudgetInvalid && isFirstClicked}
+				class:text-red-600={initialBudgetInvalid && isFirstClicked}
+				class:bg-red-100={initialBudgetInvalid && isFirstClicked}
+				bind:value={initialBudget}
+			/>
+		</div>
+		<div
+			class="bg-black w-[29%] h-7   text-white px-2 text-center text-sm tracking-wide rounded-l-[4px] overflow-hidden flex items-center justify-center font-mono font-bold"
+		>
+			<span>$</span>
+		</div>
 	</div>
 
 	<button
