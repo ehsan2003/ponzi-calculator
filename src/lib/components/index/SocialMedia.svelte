cc49e757b55a753e2f77596b218c0ea34edb0f62
ehsan <ehsan2003.2003.382@gmail.com>
2022-04-29 10:52:02 +0430

    corrects social media links

--- src/lib/components/index/SocialMedia.svelte
+++ src/lib/components/index/SocialMedia.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script>
 	import Instagram from 'svelte-material-icons/Instagram.svelte';
 	import Telegram from 'svelte-material-icons/Telegram.svelte';
 	import Twitter from 'svelte-material-icons/Twitter.svelte';
@@ -10,7 +10,7 @@
 		<hr class="h-[4px] bg-slate-5000 my-5 mx-auto grow-[1]" />
 	</div>
 	<div class="grid grid-cols-3 sm:gap-10 gap-4 justify-center items-center mt-4">
-		<a class="group" href="https://www.instagram.com/m.jorjandi">
+		<a class="group" href="https://instagram.com/m.jorjandi">
 			<div
 				class="w-full group-hover:bg-zinc-200 sm:rounded-2xl rounded-full  border border-black flex items-center justify-center aspect-square sm:text-5xl text-3xl"
 			>
@@ -30,7 +30,7 @@
 				Telegram
 			</p>
 		</a>
-		<a class="group" href="https://twitter.com/idnajroj">
+		<a class="group" href="https://twitter.com/lisa_loo_who">
 			<div
 				class="w-full group-hover:bg-zinc-200 sm:rounded-2xl rounded-full  border border-black flex items-center justify-center aspect-square sm:text-5xl text-3xl"
 			>
